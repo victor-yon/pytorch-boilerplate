883b3e318cdde4255670ca8297406ec2ddb72b13
victor-yon <victor.yon@usherbrooke.ca>
2021-02-16 17:59:13 -0500

    Make the progress bar more generic

--- utils/progress-bar.py
+++ utils/progress-bar.py
@@ -1,7 +1,7 @@
 import time
 from dataclasses import dataclass
 from random import random
-from typing import Any, Optional
+from typing import Any, Iterable, Optional
 
 from utils.timer import duration_to_str
 
@@ -13,7 +13,7 @@ class ProgressBarMetrics:
     last_printed_value: Optional[float] = None
     print_type: str = 'f'  # Accept f (float) or %
     more_is_good: bool = True
-    _printed: bool = False
+    _is_printed: bool = False
 
     def update(self, value: float) -> None:
         """
@@ -22,20 +22,19 @@ class ProgressBarMetrics:
         """
         if value is not None:
             # Keep the last value until then so we can always show the good indicator
-            if self._printed:
+            if self._is_printed:
                 self.last_printed_value = self.last_value
-            self._printed = False
+            self._is_printed = False
             self.last_value = value
 
     def printed(self) -> None:
         """ Register than the current value was printed (useful for indicators) """
-        self._printed = True
+        self._is_printed = True
 
     def evolution_indicator_str(self) -> str:
         """
         Return a colored character depending of the direction of the value.
         """
-        # TODO global variable for colors
         good_color = '\033[0;92m'  # Green text
         bad_color = '\033[0;91m'  # Red text
         no_evolution_color = '\033[0;33m'  # Orange text
@@ -64,62 +63,72 @@ class ProgressBarMetrics:
 
 
 class ProgressBar:
-    def __init__(self, nb_epoch_batch: int, nb_epoch: int, task_name: str = 'progress', length: int = 50,
-                 epoch_char: str = '⎼', fill_char: str = ' ', refresh_time: int = 500, auto_display: bool = True,
-                 with_accuracy: bool = False):
+    def __init__(self, tasks_size: int, nb_subtasks: int = 1, task_name: str = 'progress', subtask_name: str = '',
+                 metrics: Iterable[ProgressBarMetrics] = tuple(), bar_length: int = 50, subtask_char: str = '⎼',
+                 fill_char: str = ' ', refresh_time: int = 500, auto_display: bool = True):
         """
         Create a machine learning progress bar to visual print and tracking progress.
 
-        :param nb_epoch_batch: The number of batch to process per epoch.
-        :param nb_epoch: The number of epoch.
+        :param tasks_size: The number of iterations before to reach the end of the task, or the end of a subtask if
+        there is several of them.
+        :param nb_subtasks: The the number of subtasks, each subtask will have the same size (tasks_size).
         :param task_name: The name of the task.
-        :param length: The size of the visual progress bar (number of characters)
-        :param epoch_char: The character used for epoch progress done.
+        :param bar_length: The size of the visual progress bar (number of characters)
+        :param subtask_char: The character used for epoch progress done.
         :param fill_char: The character used for epoch progress pending.
         :param refresh_time: The minimal time delta between two auto print, 0 for all auto print (in milliseconds).
         :param auto_display: If true the bar will be automatically printed at the start, the end and after every value
         update if the minimal refresh time allow it.
         """
-        self.nb_epoch_batch = nb_epoch_batch
-        self.current_batch = 0
+        self.tasks_size = tasks_size
+        self.current_task_progress = 0
 
-        self.nb_epoch = nb_epoch
-        self.current_epoch = 0
+        self.nb_subtasks = nb_subtasks
+        self.current_subtask = 0
 
         self.task_name = task_name
-        self.length = length
-        self.epoch_char = epoch_char
+        self.subtask_name = subtask_name
+        self.bar_length = bar_length
+        self.task_char = subtask_char
         self.fill_char = fill_char
 
         self._start_time = None
         self._end_time = None
         self._last_print = None
-        self._refresh_time = refresh_time
+        self._refresh_time = refresh_time / 1_000  # Convert as second
         self._auto_display = auto_display
 
-        self.loss = ProgressBarMetrics('loss', more_is_good=False)
-        self.with_accuracy = with_accuracy
-        self.accuracy = ProgressBarMetrics('accuracy', print_type='%')
+        self.metrics = {metric.name: metric for metric in metrics}
 
-    def incr_batch(self, loss: Optional[float] = None, accuracy: Optional[float] = None) -> None:
+    def update(self, **metrics: Any):
         """
-        Increase by one the number of batch.
+        Update one or several metric values.
+        Every metric should have been define at the bar initialisation.
         Print the bar if auto display is enable and the minimal refresh time allow it.
-        :param loss: The loss for this batch
-        :param accuracy: The accuracy for this loss
         """
-        self.loss.update(loss)
-        self.accuracy.update(accuracy)
-        self.current_batch += 1
+        for metric, value in metrics.items():
+            assert metric in self.metrics, f'Unknown metric "{metric}", impossible to update'
+            self.metrics[metric].update(value)
+
         if self._auto_display:
             self.lazy_print()
 
-    def incr_epoch(self) -> None:
+    def incr(self, **metrics: Any) -> None:
         """
-        Increase by one the number of epoch.
+        Increase the progression of the current subtask.
         Print the bar if auto display is enable and the minimal refresh time allow it.
         """
-        self.current_epoch += 1
+        self.current_task_progress += 1
+        self.update(**metrics)
+        if self._auto_display:
+            self.lazy_print()
+
+    def incr_subtask(self) -> None:
+        """
+        Increase by one the number of the current subtask.
+        Print the bar if auto display is enable and the minimal refresh time allow it.
+        """
+        self.current_subtask += 1
         if self._auto_display:
             self.lazy_print()
 
@@ -140,63 +149,17 @@ class ProgressBar:
         """
         :return: The completed percentage of this task.
         """
-        return self.current_batch / (self.nb_epoch * self.nb_epoch_batch)
+        return self.current_task_progress / (self.nb_subtasks * self.tasks_size)
 
-    def get_epoch_progress(self) -> float:
+    def get_subtask_progress(self) -> float:
         """
         :return: The completed percentage of the current epoch.
         """
-        progress = (self.current_batch % self.nb_epoch_batch) / self.nb_epoch_batch
+        progress = (self.current_task_progress % self.tasks_size) / self.tasks_size
         # Keep 0% for the start but change to 100% for the end of each sub tasks
-        if progress == 0 and self.current_epoch != 0:
+        if progress == 0 and self.current_subtask != 0:
             return 1.0
         return progress
-
-    def print(self) -> None:
-        """ Force print the progression. """
-        print(f'{self}', end='\r', flush=True)
-        self._last_print = time.perf_counter()
-        self.loss.printed()
-        self.accuracy.printed()
-
-    def lazy_print(self) -> None:
-        """ Print the bar if the minimal refresh time allow it. """
-        if self._last_print is None or self._refresh_time == 0 or \
-                (time.perf_counter() - self._last_print) * 1_000 >= self._refresh_time:
-            self.print()
-
-    def __str__(self) -> str:
-        """
-        :return: The progress bar formatted as a string.
-        """
-        # Epoch progress
-        epoch_progress = self.get_epoch_progress()
-        nb_fill_char = int(epoch_progress * self.length)
-        bar = self.epoch_char * nb_fill_char + self.fill_char * (self.length - nb_fill_char)
-
-        # Global task
-        task_progress = self.get_task_progress()
-        nb_task_char = int(task_progress * self.length)
-        # Gray background for task loading
-        bar = '\033[0;100m' + bar[:nb_task_char] + '\033[0m' + bar[nb_task_char:]
-
-        string = f'{self.task_name}⎹ {task_progress:7.2%}⎹{bar}⎸' \
-                 f'ep. {self.current_epoch}/{self.nb_epoch} {epoch_progress:<4.0%}' \
-                 f'⎹ {self.loss}'
-
-        if self.with_accuracy:
-            string += f'⎹ {self.accuracy}'
-
-        if self._end_time is None:
-            # Still in progress
-            eta = duration_to_str(self.get_eta(), precision='s')
-            string += f'⎹ ETA: {eta}'
-        else:
-            # Task ended or interrupted
-            duration = duration_to_str(self._end_time - self._start_time, precision='s')
-            string += f'⎹ {duration}'
-
-        return string
 
     def start(self):
         """
@@ -223,7 +186,6 @@ class ProgressBar:
         self._end_time = time.perf_counter()
         if self._auto_display:
             self.print()
-            # TODO print a summary for the last one
             print()  # New line at the end
 
     def __exit__(self, *exc_info: Any) -> None:
@@ -232,17 +194,98 @@ class ProgressBar:
         """
         self.stop()
 
+    def print(self) -> None:
+        """ Force print the progression. """
+        print(f'{self}', end='\r', flush=True)
+        self._last_print = time.perf_counter()
+        for metric in self.metrics.values():
+            metric.printed()
+
+    def lazy_print(self) -> None:
+        """ Print the bar if the minimal refresh time allow it. """
+        if self._last_print is None or self._refresh_time == 0 or \
+                (time.perf_counter() - self._last_print) >= self._refresh_time:
+            self.print()
+
+    def __str__(self) -> str:
+        """
+        :return: The progress bar formatted as a string.
+        """
+        if self.nb_subtasks > 1:
+            # Subtask progress
+            subtask_progress = self.get_subtask_progress()
+            subtask_formatted_name = self.subtask_name + ' ' if self.subtask_name else ''
+            nb_fill_char = int(subtask_progress * self.bar_length)
+            bar = self.task_char * nb_fill_char + self.fill_char * (self.bar_length - nb_fill_char)
+        else:
+            bar = self.fill_char * self.bar_length
+
+        # Global task
+        task_progress = self.get_task_progress()
+        nb_task_char = int(task_progress * self.bar_length)
+        # Gray background for task loading
+        bar = '\033[0;100m' + bar[:nb_task_char] + '\033[0m' + bar[nb_task_char:]
+
+        # Progress bar
+        string = f'{self.task_name}⎹ {task_progress:7.2%}⎹{bar}⎸'
+
+        # Multiple subtask information
+        if self.nb_subtasks > 1:
+            string += f'{subtask_formatted_name}{self.current_subtask}/{self.nb_subtasks} {subtask_progress:<4.0%}⎹ '
+
+        # Metrics information
+        if len(self.metrics) > 0:
+            string += '⎹ '.join(map(str, self.metrics.values())) + '⎹ '
+
+        # Time information
+        if self._end_time is None:
+            # Still in progress
+            eta = duration_to_str(self.get_eta(), precision='s')
+            string += f'ETA: {eta}'
+        else:
+            # Task ended or interrupted
+            duration = duration_to_str(self._end_time - self._start_time, precision='s')
+            string += f'{duration}'
+
+        return string
+
+
+class ProgressBarTraining(ProgressBar):
+    def __init__(self, nb_batch: int, nb_epoch: int):
+        super().__init__(nb_batch, nb_epoch, 'Training', 'ep.', metrics=(
+            ProgressBarMetrics('loss', more_is_good=False),
+            ProgressBarMetrics('accuracy', print_type='%')
+        ))
+
+
+class ProgressBarTesting(ProgressBar):
+    def __init__(self, nb_batch: int):
+        super().__init__(nb_batch, 1, 'Testing ')
+
 
 if __name__ == '__main__':
     nb_batch = 10
     nb_epoch = 2
 
-    with ProgressBar(nb_batch, nb_epoch, refresh_time=200, with_accuracy=True) as p:
+    print('start')
+
+    with ProgressBarTraining(nb_batch, nb_epoch) as p:
         for epoch_i in range(nb_epoch):
-            p.incr_epoch()
+            p.incr_subtask()
             for batch in range(nb_batch):
                 # Do stuff...
                 time.sleep(1)
-                p.incr_batch(loss=batch, accuracy=random())
+                # if batch < 5:
+                #     print(f'Batch {batch}')
+                p.incr(loss=batch, accuracy=random())
+
+    print('end')
+
+    print('start')
+    with ProgressBarTesting(nb_batch) as p:
+        for batch in range(nb_batch):
+            # Do stuff...
+            time.sleep(1)
+            p.incr()
 
     print('end')
